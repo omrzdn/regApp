6d5aeb82c0af9c621ab8a2645610446b87f74359
Omar Zidan <th3develop@gmail.com>
2020-03-17 00:50:15 +0200

    Created the login page and i did  some validation on the registration page

---
+++ control/login.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once '../inc/connection.php';
+
+if(isset($_POST['username'], $_POST['password'])){
+
+  
+}
--- control/register.php
+++ control/register.php
@@ -4,26 +4,61 @@ require_once "../inc/connection.php";
 
 if(isset($_POST['username'], $_POST['password'], $_POST['password_confirm'], $_POST['email'])){
 
-  $username         = $_POST['username'];
-  $password         = $_POST['password'];
-  $password_confirm = $_POST['password_confirm'];
-  $email            = $_POST['email'];
+    $username         = $_POST['username'];
+    $password         = $_POST['password'];
+    $password_confirm = $_POST['password_confirm'];
+    $email            = $_POST['email'];
 
-  if(preg_match('/^[a-z0-9_.]*$/i',$username)){
-    if(strlen($password) >= 8 && strlen($password) <= 32){
-      if($password_confirm === $password){
-        if (FILTER_VALIDATE_EMAIL($email)) {
-            $stmt = $pdo->prepare('INSERT INTO ');
+    if(preg_match('/^[a-z0-9_.]*$/i',$username)){
+
+        if(strlen($password) >= 8 && strlen($password) <= 32){
+
+            if($password_confirm === $password){
+
+              if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+                  $stmt = $pdo->prepare('SELECT * FROM users WHERE username = ?');
+                  $stmt->execute([$username]);
+
+                  if($stmt->rowCount()){
+
+                    die('Username is already taken, please pick up anthor one.');
+
+                  }else {
+
+                    $stmt = $pdo->prepare('SELECT * FROM users WHERE email = ?');
+                    $stmt->execute([$email]);
+
+                    if($stmt->rowCount()){
+
+                      die('Email is already taken, please pick up anthor one.');
+
+                    }else {
+
+                      $stmt = $pdo->prepare('INSERT INTO users(`username`,`password`,`email`) VALUES (?,?,?)');
+                      $stmt->execute([
+                        $username,
+                        password_hash($password,PASSWORD_DEFAULT,['cost' => 11]),
+                        $email
+                      ]);
+
+                      if($stmt->rowCount()){
+
+                        echo "Thank you for registration, please go to active your account. ";
+                      }
+                    }
+                  }
+
+              }else{
+                echo 'please provide a valide email';
+                }
+          }else{
+
+            echo 'password confirmation doesn\'t match';
+          }
         }else {
-          echo 'please provide a valide email';
+        echo 'your password must be bigger than 8 charcters and smaller than 32';
         }
-      }else{
-
-        echo 'password confirmation doesn\'t match';
-      }
-    }else {
-      echo 'your password must be bigger than 8 charcters and smaller than 32';
-    }
   }else{
 
     echo 'please provide a valide username';
